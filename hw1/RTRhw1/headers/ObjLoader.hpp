091ae86447957817a0019c1344968e8f47fed425
yanagiragi <qaz90473@gmail.com>
2017-11-11 21:06:01 +0800

    update normal color

--- hw1/RTRhw1/headers/ObjLoader.hpp
+++ hw1/RTRhw1/headers/ObjLoader.hpp
@@ -12,12 +12,17 @@ using glm::vec3;
 using std::string;
 using std::vector;
 
+struct Vertex {
+	vec3 positions;
+	vec3 normals;
+};
+
 class ObjLoader
 {
 	public:
-		vector<vec3> vertices;
-		vector<vec3> normals;
-		vector<vec3> indices;
+		
+		vector<struct Vertex> vertices;
+		vector<unsigned int> indices;
 	
 		vector<string> rawData;
 
@@ -31,24 +36,37 @@ class ObjLoader
 					
 					std::vector<std::string> v = Utils::split(buffer.substr(string("v ").size(), buffer.length() - string("v ").size()), ' ');
 					
-					auto vertex = vec3(
+					auto pos = vec3(
 						std::stof(v[0], nullptr),
 						std::stof(v[1], nullptr),
 						std::stof(v[2], nullptr)
 					);
 					
-					vertices.push_back(vertex);
+					struct Vertex vert;
+					vert.positions = pos;
+
+					vertices.push_back(vert);
 				}
 				else if (buffer[0] == 'f') {
 					std::vector<std::string> f = Utils::split(buffer.substr(string("f ").size(), buffer.length() - string("f ").size()), ' ');
 
-					auto faces = vec3(
-						std::stod(f[0], nullptr),
-						std::stod(f[1], nullptr),
-						std::stod(f[2], nullptr)
-					);
+					unsigned int f0 = std::stod(f[0], nullptr) - 1;
+					unsigned int f1 = std::stod(f[1], nullptr) - 1;
+					unsigned int f2 = std::stod(f[2], nullptr) - 1;
 
-					indices.push_back(faces);
+					indices.push_back(f0);
+					indices.push_back(f1);
+					indices.push_back(f2);
+					
+					auto v1 = vertices[f0].positions;
+					auto v2 = vertices[f1].positions;
+					auto v3 = vertices[f2].positions;
+
+					auto n = glm::cross(v2 - v1, v3 - v1);
+
+					vertices[f0].normals = n;
+					vertices[f1].normals = n;
+					vertices[f2].normals = n;
 				}
 			}
 		}
--- hw1/RTRhw1/src/Behaviour.hpp
+++ hw1/RTRhw1/src/Behaviour.hpp
@@ -12,30 +12,25 @@
 class Behaviour 
 {
 	public:
-		const float vertices[12] = {
-			0.5f,  0.5f, 0.0f,  // top right
-			0.5f, -0.5f, 0.0f,  // bottom right
-			-0.5f, -0.5f, 0.0f,  // bottom left
-			-0.5f,  0.5f, 0.0f   // top left 
-		};
 
-		const unsigned int indices[6] = {
-			0, 1, 3,
-			1, 2, 3
-		};
+		/* Variables */
 
 		const char* vertexShaderSource = "#version 330 core\n"
-			"layout (location = 0) in vec3 aPos;\n"
+			"layout (location = 0) in vec3 pos;\n"
+			"layout (location = 0) in vec3 normal;\n"
+			"out vec4 ourColor;\n"
 			"void main()\n"
 			"{\n"
-			"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+			"   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
+			"	ourColor = vec4(normal.x, normal.y, normal.z, 0.0);"
 			"}\0";
 
 		const char* fragmentShaderSource = "#version 330 core\n"
 			"out vec4 FragColor;\n"
-			"uniform vec4 ourColor;\n"
+			"in vec4 ourColor;\n"
 			"void main()\n"
 			"{\n"
+			"	vec4 Red = vec4(1.0, 0.0, 0.0, 1.0);"
 			"   FragColor = ourColor;\n"
 			"}\n\0";
 
@@ -43,13 +38,16 @@ class Behaviour
 		unsigned int vertexShader;
 		unsigned int fragmentShader;
 		unsigned int shaderProgram;
+		
+		// Load & Parse Obj
+		ObjLoader loader = ObjLoader("Resources/teapot.obj");
 
-		// Prepare data
-		void Behaviour :: Start()
+
+		/* Functions */
+
+		void Behaviour::setupShader() 
 		{
-			ObjLoader loader("Resources/teapot.obj");
-			//Reader r("src/Behaviour.hpp");
-
+			// Create Shader program and setup buffers
 			Shader generator = Shader();
 
 			generator.CreateShader(vertexShader, GL_VERTEX_SHADER, vertexShaderSource);
@@ -71,16 +69,28 @@ class Behaviour
 
 			// copy vertex data into buffer's memory
 			// GL_STATIC_DRAW: the data will most likely not change at all or very rarely.
-			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+			glBufferData(GL_ARRAY_BUFFER, loader.vertices.size() * sizeof(Vertex), &(loader.vertices[0]), GL_STATIC_DRAW);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, loader.indices.size() * sizeof(unsigned int), &(loader.indices[0]), GL_STATIC_DRAW);
 
-			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+			// ¼È®É¥ý¤£¥ánormal
+
 			// 0 is the index of vertex attribute
 			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(struct Vertex), (void *)0);
+
+			// vertex normals
+			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(struct Vertex), (void*)offsetof(struct Vertex, normals));
 
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
 			glBindVertexArray(0);
+		}
 
+		// Prepare data
+		void Behaviour :: Start()
+		{
+			// Setup Shader, VAO, VBO, EBO
+			setupShader();
 		}
 
 		void Behaviour :: Update()
@@ -100,10 +110,10 @@ class Behaviour
 			int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 
 			glUseProgram(shaderProgram);
-			glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+			//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
 			glBindVertexArray(VAO);
-			glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+			glDrawElements(GL_TRIANGLES, loader.indices.size(), GL_UNSIGNED_INT, 0);
 		}
 
 		// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
